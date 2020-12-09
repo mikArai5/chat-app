90b6a70e1f46d5214c04b8f57b30e436422d55c1
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2020-12-09 22:00:26 +0900

    メッセージ送信機能の実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,4 @@
 class RoomUser < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
-  has_many :messages
-
-  validates :name, presence: true
+  belongs_to :room
+  belongs_to :user
 end
